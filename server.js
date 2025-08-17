86371a09da879526e23e0bb6a9ca2bd4bfaf44bf
Shraideh2000 <mohashrd9@gmail.com>
2025-08-17 03:19:31 +0300

    fix

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ import { Pool } from "pg";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-// import dotenv from "dotenv";
-
-// تهيئة dotenv لقراءة متغيرات البيئة المحلية
-// dotenv.config();
 
 // مسارات Node.js
 const __filename = fileURLToPath(import.meta.url);
